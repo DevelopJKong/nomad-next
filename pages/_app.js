eb3722cf6077a09f113a97ca0728b40a7bc4591b
DevelopJKong <ybaram40@gmail.com>
2022-05-19 21:52:00 +0900

    #1.6 Custom App 완료

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import NavBar from "../components/Navbar";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>
+        {`
+          a {
+            color: white;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/Navbar";
+import Potato from "./about";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>Home</h1>
+    </div>
+  );
+}
